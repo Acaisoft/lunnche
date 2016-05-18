7534675c04e751cd5c01c7d39da548b5d24d43d6
Adam Nowaczyk <adam.nowaczyk@acaisoft.com>
2016-05-18 21:37:10 +0200

    Added initial test data when running application

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO restaurant (id, name) VALUES (1, 'Restaurant #1');
+INSERT INTO restaurant (id, name) VALUES (2, 'Restaurant #2');
+
+INSERT INTO meal (id, name, price, restaurant_id) VALUES (1, 'Meal #11', 10, 1);
+INSERT INTO meal (id, name, price, restaurant_id) VALUES (2, 'Meal #12', 20, 1);
+
+INSERT INTO meal (id, name, price, restaurant_id) VALUES (3, 'Meal #21', 15, 2);
+INSERT INTO meal (id, name, price, restaurant_id) VALUES (4, 'Meal #22', 25, 2);
+
+INSERT INTO user (id, first_name, last_name, balance, username, password)
+VALUES (1, 'Jan', 'Kowalski', 50, 'jan.kowalski', 'password');
+INSERT INTO user (id, first_name, last_name, balance, username, password)
+VALUES (2, 'Stefan', 'Nowak', 100, 'stefan.nowak', 'password');
+
+INSERT INTO purchase (id) VALUES (1);
+INSERT INTO purchase (id) VALUES (2);
+
+INSERT INTO purchase_order (id, user_id, meal_id, purchase_id) VALUES (1, 1, 1, 1);
+INSERT INTO purchase_order (id, user_id, meal_id, purchase_id) VALUES (2, 2, 2, 1);
+
+INSERT INTO purchase_order (id, user_id, meal_id, purchase_id) VALUES (3, 1, 4, 2);
+
