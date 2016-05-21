a217f5012ebd6f03231da8f922bd58d68faf8dfb
hiszpan <veakabori@gmail.com>
2016-05-21 19:16:31 +0100

    Deleting unnecessary files

--- src/main/resources/src/app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var browser_1 = require('angular2/platform/browser');
-var router_1 = require("angular2/router");
-var app_component_1 = require('./app.component');
-var common_1 = require("angular2/common");
-browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS, common_1.FORM_PROVIDERS]);
-//# sourceMappingURL=main.js.map
--- src/main/resources/src/app/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    public firstName: string;
-    public lastName: string;
-    public balance: number;
-    public username: string;
-    public password: string;
-    
-}
